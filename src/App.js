a3d08f20bb3e54fda06727eeff99613438b133ab
Remi Mendoza <mendozaremi@gmail.com>
2021-03-04 14:36:01 -0500

    completed search component and basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SearchMovies from './components/SearchMovies'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello WOrld</h1>
+      <h1 className="title">The Shoppies 🍿</h1>
+      <SearchMovies/>
     </div>
   );
 }
