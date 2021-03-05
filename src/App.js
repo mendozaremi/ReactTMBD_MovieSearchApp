d06ef5af7852084ff49468ad7e28ccb0e42eed21
Remi Mendoza <mendozaremi@gmail.com>
2021-03-04 23:27:02 -0500

    pre-deployment and css styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import SearchMovies from './components/SearchMovies'
+import Header from './ui/Header'
+import Footer from './ui/Footer'
 
 function App() {
   return (
+    <>
+    <Header/>
     <div className="App">
       <h1 className="title">The Shoppies 🍿</h1>
       <SearchMovies/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
